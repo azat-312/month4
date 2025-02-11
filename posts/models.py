69f4cba7673d6116ce0882b4720dba4570f55ef0
azat-312 <azat.myrzabekov05@gmail.com>
2025-02-11 15:52:12 +0600

    home work3

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag (models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+    
+
 class Post(models.Model):
+    image = models.ImageField(null=True,blank=True)
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=500)
     rate = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
-
+    category = models.ForeignKey(Category,null=True,
+            on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag , null=True,blank=True )
 
     def __str__(self):
         return f"{self.title} - {self.content}"
